00c0fe2291d91f996b41ec9e75e7ccd4b307e3f3
mikemct <m.mctighe10@gmail.com>
2022-12-06 21:14:57 -0500

    robit AI skynet babbeeeeeeeeeeeeeeeeeeeeeeeee

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 print("hello world")
 # the run command for python files is python3 "the file name".py 
 # welcome back to earth lol 
+# ---------------------------------------------------------------------------------
+
+from transformers import pipeline
+
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
+
+# prompt = "The New York Jets"
+prompt = "New York Yankees"
+
+result = generator(prompt, max_length=25, do_sample=True, temperature=0.9)
+
+print(result[0]['generated_text'])
