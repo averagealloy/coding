f8d7f62c6227c87abd2e29e3a96d9dcf5c4195cd
mikemct <m.mctighe10@gmail.com>
2022-12-07 23:48:19 -0500

    the code says it all

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ print("hello world")
 # the run command for python files is python3 "the file name".py 
 # welcome back to earth lol 
 # ---------------------------------------------------------------------------------
+# need to add more explanation for the below steps. Just super tired today.
 
 from transformers import pipeline
 
@@ -13,3 +14,4 @@ prompt = "New York Yankees"
 result = generator(prompt, max_length=25, do_sample=True, temperature=0.9)
 
 print(result[0]['generated_text'])
+
