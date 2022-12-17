8aa069e3b63deff1064a968e51862fe8af6975e0
mikemct <m.mctighe10@gmail.com>
2022-12-16 19:59:07 -0500

    now I am tweeting from twitters api

--- apitest.py
+++ apitest.py
@@ -1,4 +1,9 @@
 import creds
+import tweepy
 
 print('hello')
 
+client = tweepy.Client(creds.bearer_token, creds.api_key, creds.api_key_secret, creds.access_token, creds.access_token_secret)
+
+client.create_tweet(text='this is a test tweet from twitters api really')
+print('made a tweet')
