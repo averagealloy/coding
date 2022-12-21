75726f2e96c904e8108b0f4e0893bf91011221f2
mikemct <m.mctighe10@gmail.com>
2022-12-21 13:15:03 -0500

    just making sure it still works

--- apitest.py
+++ apitest.py
@@ -5,5 +5,5 @@ print('hello')
 
 client = tweepy.Client(creds.bearer_token, creds.api_key, creds.api_key_secret, creds.access_token, creds.access_token_secret)
 
-client.create_tweet(text='this is a test tweet from twitters api really')
+client.create_tweet(text='still works lol')
 print('made a tweet')
