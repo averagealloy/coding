d9cd1a76a94a5abba328f4e45bf1276b13f4d844
mikemct <m.mctighe10@gmail.com>
2022-12-21 16:44:29 -0500

    removed upper

--- aitest.py
+++ aitest.py
@@ -11,6 +11,6 @@ result = generator(prompt, max_length=25, do_sample=True, temperature=0.9)
 
 # print(result[0]['generated_text'])
 # we want the 0th item in the the list. Then we want to get the item in the dict with the key called "generated text"
-print(result[0]['generated_text'].upper())
+print(result[0]['generated_text'])
 
 justText = result[0]['generated_text']
